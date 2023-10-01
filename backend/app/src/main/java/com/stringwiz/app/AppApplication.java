809a79395dc7e830ea539d6bde05ae5640b8bbcd
mariobd38 <mariobd38@gmail.com>
2023-09-30 20:00:16 -0700

    testing commit

--- backend/app/src/main/java/com/stringwiz/app/AppApplication.java
+++ backend/app/src/main/java/com/stringwiz/app/AppApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppApplication {
 
 	public static void main(String[] args) {
-		System.out.println();
 		SpringApplication.run(AppApplication.class, args);
 	}
 
